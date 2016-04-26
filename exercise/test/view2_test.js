4e2f7ab2c71b63f97cdfac927381302d79617436
alvaro.delaserna <alvaro.delaserna@bbva.com>
2016-04-26 10:44:39 +0200

    ADD tests for traversing the loop

--- exercise/test/view2_test.js
+++ exercise/test/view2_test.js
@@ -12,10 +12,6 @@ describe('Controller: View2Ctrl', function () {
 
 		module('view2');
 
-// 		module(function ($provide) {
-// 			$provide.value('ServiceOne', jasmine.createSpyObj('ServiceOne', ['createPromise', 'setVariable', 'getVariable']));
-// 		});
-
 		inject(function ($controller, _$rootScope_,	_$timeout_, _$q_, _ServiceOne_) {
             $scope = _$rootScope_.$new();
 			$q = _$q_;
@@ -30,6 +26,52 @@ describe('Controller: View2Ctrl', function () {
 			});
 		});
 
+	});
+
+	describe('ServiceOne.createPromise', function () {
+		beforeEach(function () {
+			ServiceOne.createPromise.and.callFake(function () {
+				deferred = $q.defer();
+				if (arguments[0]) {
+					deferred.resolve('Completed');
+				} else {
+					deferred.reject('Failed');
+				}
+				return deferred.promise;
+			});
+		});
+
+		it('traverses "real" loop', function () {
+			expect(View2Ctrl.state).not.toBeDefined();
+			expect(View2Ctrl.i).toEqual(1);
+			View2Ctrl.loop();
+			expect(View2Ctrl.state).toEqual('Executing...');
+			expect(ServiceOne.createPromise).toHaveBeenCalledWith(true);
+
+			$timeout.flush();
+
+			expect(ServiceOne.createPromise).toHaveBeenCalledWith(true);
+			expect(View2Ctrl.promise1).toEqual('Completed');
+
+			$timeout.flush();
+
+			expect(View2Ctrl.promise2).toEqual('Completed');
+
+			$timeout.flush();
+			expect(ServiceOne.createPromise).toHaveBeenCalledWith(false);
+			expect(View2Ctrl.promise3).toEqual('Failed');
+
+			$timeout.flush();
+			expect(ServiceOne.createPromise).toHaveBeenCalledWith(true);
+			expect(View2Ctrl.promise4).toEqual('Completed');
+
+			$timeout.flush();
+			expect(ServiceOne.createPromise).toHaveBeenCalledWith(true);
+			expect(View2Ctrl.promise5).toEqual('Completed');
+
+			$timeout.flush();
+			expect(View2Ctrl.state).toEqual('Done!');
+		});
 	});
 
 	describe('auto loop', function () {
@@ -51,32 +93,30 @@ describe('Controller: View2Ctrl', function () {
 			$timeout.flush();
 			
 			expect(View2Ctrl.i).toEqual(2);
-			expect(View2Ctrl.state).toEqual('Executing...');
 			expect(View2Ctrl.promise1).toEqual('Completed');
 
 			$timeout.flush();
 
 			expect(View2Ctrl.i).toEqual(3);
-			expect(View2Ctrl.state).toEqual('Executing...');
-			expect(View2Ctrl.promise1).toEqual('Completed');
 			expect(View2Ctrl.promise2).toEqual('Completed');
 
 			$timeout.flush();
 
 			expect(View2Ctrl.i).toEqual(4);
-			expect(View2Ctrl.state).toEqual('Executing...');
-			expect(View2Ctrl.promise1).toEqual('Completed');
-			expect(View2Ctrl.promise2).toEqual('Completed');
 			expect(View2Ctrl.promise3).toEqual('Completed');
 
 			$timeout.flush();
 
 			expect(View2Ctrl.i).toEqual(5);
-			expect(View2Ctrl.state).toEqual('Executing...');
-			expect(View2Ctrl.promise1).toEqual('Completed');
-			expect(View2Ctrl.promise2).toEqual('Completed');
-			expect(View2Ctrl.promise3).toEqual('Completed');
 			expect(View2Ctrl.promise4).toEqual('Completed');
+
+			$timeout.flush();
+
+			expect(View2Ctrl.i).toEqual(6);
+			expect(View2Ctrl.promise5).toEqual('Completed');
+
+			$timeout.flush();
+			expect(View2Ctrl.state).toEqual('Done!');
 		});
 
 		it('should traverse loop and reject all promises', function () {
@@ -121,7 +161,7 @@ describe('Controller: View2Ctrl', function () {
 		});
 	});
 
-	describe('handling service variable', function (){
+	describe('handling service variable: setServiceVariableAndPrintIt', function (){
 		var VALUE = 'abc';
 		beforeEach(function () {
 			ServiceOne.getVariable.and.returnValue(VALUE);
